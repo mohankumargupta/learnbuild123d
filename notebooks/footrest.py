07077f2e2a235b85d65eb58b1f59eb5be73e95eb
Raspberry Pi <raspberrypisig@gmail.com>
2024-12-19 14:33:52 +1100

    footrest making progress

--- notebooks/footrest.py
+++ notebooks/footrest.py
@@ -5,25 +5,29 @@ LENGTH = 220
 WIDTH = 70
 HEIGHT = 50
 
+with BuildSketch() as sketch:
+    with BuildLine() as line:
+        l1 = Line((0,50),(70,50))
+        l2 = Line(l1@0, (0,0))
+        l3 = PolarLine(l1@1, length=7, direction=(-1.0,-50./70), length_mode=LengthMode.DIAGONAL)
+        l4 = PolarLine(l2@1, length=7, direction=(1.0,50./70), length_mode=LengthMode.DIAGONAL)
+        l5 = SagittaArc(l3@1, l4@1, sagitta=-5.531)
+    face = make_face()
+
 with BuildPart() as footrest_builder:
     with BuildSketch(Plane.YZ) as side_sketch:
         with BuildLine() as side_lines:
-            '''
-            pts = [
-                (0,0),
-                (70,0),
-                (0,50),
-                (0,0)
-            ]
-            Polyline(pts)
-            '''
             l1 = Line((0,50),(70,50))
-            l2 = Line(l1@1, (0,0))
-            Line(l2@1, l1@0)
-        make_face()
+            l2 = Line(l1@0, (0,0))
+            l3 = PolarLine(l1@1, length=7, direction=(-1.0,-50./70), length_mode=LengthMode.DIAGONAL)
+            l4 = PolarLine(l2@1, length=7, direction=(1.0,50./70), length_mode=LengthMode.DIAGONAL)
+            l5 = SagittaArc(l3@1, l4@1, sagitta=-5.531)
+        face = make_face()
     extrude(amount=LENGTH)
 footrest = footrest_builder.part
 footrest
 
 # %%
 print(footrest.volume)
+export_stl(footrest, "footrest.stl")
+#print(face.area)
